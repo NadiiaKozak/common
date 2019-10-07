6a2379451e035befcf6a8c0816eaf0a0072ae454
Nadiia Kozak <lv191177anv@gmail.com>
2019-10-07 23:18:16 +0300

    add changes to tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -1,54 +1,70 @@
 import unittest
 import math
 
+
 from homework import Rectangle
+
+
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-        self.rectangle1 = Rectangle(5, 8)
-        self.rectangle2 = Rectangle(10, 4)
-        self.square = Rectangle(4, 4)
+        self.arg_list = [
+            {'args': [5, 8],
+             'perimeter': 26,
+             'square': 40,
+             'diagonal': math.sqrt(89),
+             'radius_of_circumscribed_circle': math.sqrt(89) / 2},
 
-    def test_get_rectangle_perimeter(self):
-        self.assertEqual(self.rectangle1.get_rectangle_perimeter(), 26)
-        self.assertEqual(self.rectangle2.get_rectangle_perimeter(), 28)
-        self.assertEqual(self.square.get_rectangle_perimeter(), 16)
+            {'args': [10, 4],
+             'perimeter': 28,
+             'square': 40,
+             'diagonal': math.sqrt(116),
+             'radius_of_circumscribed_circle': math.sqrt(116) / 2},
 
-    def test_get_rectangle_square(self):
-        self.assertEqual(self.rectangle1.get_rectangle_square(), 40)
-        self.assertEqual(self.rectangle2.get_rectangle_square(), 40)
-        self.assertEqual(self.square.get_rectangle_square(), 16)
+            {'args': [4, 4],
+             'perimeter': 16,
+             'square': 16,
+             'diagonal': math.sqrt(32),
+             'radius_of_circumscribed_circle': math.sqrt(32) / 2,
+             'radius_of_inscribed_circle':  math.sqrt(32) / (2 * math.sqrt(2))}
+    ]
+
+    def test_rectangle(self):
+        for i in self.arg_list:
+            val = [v for v in i.values() if isinstance(v, list)]
+            rect = Rectangle(val[0][0], val[0][1])
+            result1 = rect.get_rectangle_perimeter()
+            result2 = rect.get_rectangle_square()
+            result3 = rect.get_rectangle_diagonal()
+            result4 = rect.get_radius_of_circumscribed_circle()
+            self.assertEqual(result1, i['perimeter']) #test rectangle perimeter
+            self.assertEqual(result2, i['square'])    #test rectangle square
+            self.assertEqual(result3, i['diagonal'])  #test rectangle diagonal
+            self.assertEqual(result4, i['radius_of_circumscribed_circle']) #test radius_of_circumscribed_circle
+            if val[0][0] == val[0][1]:
+                result5 = rect.get_radius_of_inscribed_circle()
+                self.assertEqual(result5, i['radius_of_inscribed_circle'])#test radius_of_inscribed_circle
+
+            else:
+                with self.assertRaises(ValueError):
+                    rect.get_radius_of_inscribed_circle()
 
     def test_get_sum_of_corners(self):
-        self.assertEqual(self.rectangle1.get_sum_of_corners(1), 90)
-        self.assertEqual(self.rectangle1.get_sum_of_corners(2), 180)
-        self.assertEqual(self.rectangle1.get_sum_of_corners(3), 270)
-        self.assertEqual(self.rectangle1.get_sum_of_corners(4), 360)
+        rect = Rectangle(None, None)
+        self.assertEqual(rect.get_sum_of_corners(1), 90)
+        self.assertEqual(rect.get_sum_of_corners(2), 180)
+        self.assertEqual(rect.get_sum_of_corners(3), 270)
+        self.assertEqual(rect.get_sum_of_corners(4), 360)
 
     def test_get_sum_of_corners_error(self):
+        rect = Rectangle(None, None)
         with self.assertRaises(ValueError):
-            self.rectangle1.get_sum_of_corners(5)
-            self.rectangle1.get_sum_of_corners(6)
-            self.rectangle1.get_sum_of_corners(7)
-            self.rectangle1.get_sum_of_corners(8)
+            rect.get_sum_of_corners(5)
+            rect.get_sum_of_corners(6)
+            rect.get_sum_of_corners(7)
+            rect.get_sum_of_corners(8)
 
-    def test_get_rectangle_diagonal(self):
-        self.assertAlmostEqual(self.rectangle1.get_rectangle_diagonal(), math.sqrt(89), places=2)
-        self.assertAlmostEqual(self.rectangle2.get_rectangle_diagonal(), math.sqrt(116), places=2)
-        self.assertAlmostEqual(self.square.get_rectangle_diagonal(), math.sqrt(32), places=2)
 
-    def test_get_radius_of_circumscribed_circle(self):
-        self.assertAlmostEqual(self.rectangle1.get_radius_of_circumscribed_circle(), math.sqrt(89) / 2, places=2)
-        self.assertAlmostEqual(self.rectangle2.get_radius_of_circumscribed_circle(), math.sqrt(116) / 2, places=2)
-        self.assertAlmostEqual(self.square.get_radius_of_circumscribed_circle(), math.sqrt(32) / 2, places=2)
-
-    def test_get_radius_of_inscribed_circle(self):
-        self.assertAlmostEqual(self.square.get_radius_of_inscribed_circle(), math.sqrt(32) / (2 * math.sqrt(2)), places=2)
-
-    def test_get_radius_of_inscribed_circle_error(self):
-        with self.assertRaises(ValueError):
-            self.rectangle1.get_radius_of_inscribed_circle()
-            self.rectangle2.get_radius_of_inscribed_circle()
 
 if __name__ == '__main__':
     unittest.main()
