54c28462876e7868de4dcc3cc5f63e3afb8d29a0
Nadiia Kozak <lv191177anv@gmail.com>
2019-10-26 19:23:10 +0300

    add HW flask intro

--- flask1/main.py
+++ flask1/main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
-пше
+
 @app.route('/home')
 def get_home_page():
     return render_template('home.html')
@@ -9,12 +9,12 @@ def get_home_page():
 @app.route('/vegetables')
 def get_vegetables_page():
     vagetables = ['beans', 'carrot', 'beetroot', 'cucumber']
-    return render_template('vegetables.html', list = vagetables)
+    return render_template('vegetables.html', list_vagetables=vagetables)
 
 @app.route('/fruits')
 def get_fruits_page():
     fruits = ['melon', 'apple', 'strawberry', 'grape']
-    return render_template('fruits.html', list = fruits)
+    return render_template('fruits.html', list_fruits=fruits)
 
 if __name__ == "__main__":
     app.run(debug=True)
