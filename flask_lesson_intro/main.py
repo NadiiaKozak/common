4019e002800a696fddc6015c5171de78adb4b020
Nadiia Kozak <lv191177anv@gmail.com>
2019-10-28 16:14:13 +0200

    add file to flask_lesson_||

--- flask_lesson_intro/main.py
+++ flask_lesson_intro/main.py
@@ -3,10 +3,37 @@ from utils import get_data
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/home')
 def get_home_page():
-    return render_template("home.html", data=get_data())
+    return render_template("home.html")
+
+@app.route('/visa')
+def get_visa_page():
+    return render_template("visa.html", data=get_data())
+
+@app.route('/mastercard')
+def get_mastercard_page():
+    return render_template("mastercard.html", data=get_data())
+
+@app.route('/american_express')
+def get_american_express_page():
+    return render_template("american_express.html", data=get_data())
+
+@app.route('/maestro')
+def get_maestro_page():
+    return render_template("maestro.html", data=get_data())
+
+@app.route('/paypal')
+def get_paypal_page():
+    return render_template("paypal.html", data=get_data())
+
+@app.route('/discover')
+def get_discover_page():
+    return render_template("discover.html", data=get_data())
+
+@app.route('/author')
+def get_author_page():
+    return render_template("author.html")
 
 
 if __name__ == "__main__":
