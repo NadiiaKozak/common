94a9b4a9eb127482581e1e4d91d0bd139502d38d
Nadiia Kozak <lv191177anv@gmail.com>
2019-11-01 21:42:55 +0200

    add file to flask_lesson_||

--- flask_lesson_intro/main.py
+++ flask_lesson_intro/main.py
@@ -3,33 +3,65 @@ from utils import get_data
 
 app = Flask(__name__)
 
+
 @app.route('/home')
 def get_home_page():
     return render_template("home.html")
 
+
 @app.route('/visa')
 def get_visa_page():
-    return render_template("visa.html", data=get_data())
+    picture = 'visa.jpg'
+    title = 'Visa'
+    for i in get_data():
+        if i['title'] == title:
+            return render_template("visa.html", picture=picture, title=title, text=i['text'] )
+
 
 @app.route('/mastercard')
 def get_mastercard_page():
-    return render_template("mastercard.html", data=get_data())
+    picture = 'mc.png'
+    title = 'MasterCard'
+    for i in get_data():
+        if i['title'] == title:
+            return render_template("mastercard.html", picture=picture, title=title, text=i['text'])
+
 
 @app.route('/american_express')
 def get_american_express_page():
-    return render_template("american_express.html", data=get_data())
+    picture = 'american.jpg'
+    title = 'American Express'
+    for i in get_data():
+        if i['title'] == title:
+            return render_template("american_express.html", picture=picture, title=title, text=i['text'])
+
 
 @app.route('/maestro')
 def get_maestro_page():
-    return render_template("maestro.html", data=get_data())
+    picture = 'maestro.jpeg'
+    title = 'Maestro'
+    for i in get_data():
+        if i['title'] == title:
+            return render_template("maestro.html", picture=picture, title=title, text=i['text'])
+
 
 @app.route('/paypal')
 def get_paypal_page():
-    return render_template("paypal.html", data=get_data())
+    picture = 'paypal.jpg'
+    title = 'PayPal'
+    for i in get_data():
+        if i['title'] == title:
+            return render_template("paypal.html", picture=picture, title=title, text=i['text'])
+
 
 @app.route('/discover')
 def get_discover_page():
-    return render_template("discover.html", data=get_data())
+    picture = 'discover.jpeg'
+    title = 'Discover'
+    for i in get_data():
+        if i['title'] == title:
+            return render_template("paypal.html", picture=picture, title=title, text=i['text'])
+
 
 @app.route('/author')
 def get_author_page():
