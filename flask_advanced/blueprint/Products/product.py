6bbf86d03ebc1ae7cee53d126f712d08b3f40208
Nadiia Kozak <lv191177anv@gmail.com>
2019-11-05 11:24:51 +0200

    add HW3 flask

---
+++ flask_advanced/blueprint/Products/product.py
@@ -0,0 +1,65 @@
+import os
+import uuid
+
+from flask import Blueprint, render_template, request, session
+
+from .utils import get_data, add_data
+
+product = Blueprint('product', __name__, template_folder='templates', static_folder='static1')
+
+DB1 = get_data()
+
+
+@product.route('/products', methods=['GET'])
+def get_products():
+    return render_template('all_products.html', products=get_data())
+
+
+@product.route('/products', methods=['POST'])
+def search_products():
+    id_product = request.form.get('id')
+    price = request.form.get('price')
+    data = []
+    for i in get_data():
+        if i['id'] == id_product and i['price'] == price:
+            session[i['id']] = True
+            return render_template('all_products.html', products=i, link_flags=session)
+        elif i['price'] == price:
+            session[i['price']] = True
+            data.append(i)
+        elif i['id'] == id_product:
+            session[i['id']] = True
+            data.append(i)
+    return render_template('all_products.html', products=data, link_flags=session)
+
+
+@product.route('/<name_product>', methods=['GET'])
+def get_product(name_product):
+    for i in get_data():
+        if i['name'] == name_product:
+            return render_template("product.html",
+                                   picture=i['img_name'],
+                                   title=i['name'],
+                                   text=i['description'],
+                                   price=i['price'],
+                                   id_product=i['id'])
+
+
+@product.route('/add_product', methods=['GET'])
+def add_product():
+    return render_template('add_product.html')
+
+
+@product.route('/add_product', methods=['POST'])
+def add_product_post():
+    new_product = {"id": str(uuid.uuid4()), "name": request.form.get('name'),
+                   "description": request.form.get('description'), "price": request.form.get('price')}
+    image = request.files['image']
+    new_product["img_name"] = image.filename
+    path = os.path.join('blueprint/Products/static1', image.filename)
+    image.save(path)
+    DB1.append(new_product)
+    add_data(DB1)
+    return "product added"
+
+
---
+++ flask_advanced/blueprint/Supermarkets/supermarket.py
@@ -0,0 +1,61 @@
+import os
+import uuid
+
+from flask import Blueprint, render_template, request, session
+
+from .utils import get_data, add_data
+
+supermarket = Blueprint("supermarket", __name__, template_folder='templates', static_folder='static2')
+
+supermarkets = get_data()
+
+
+@supermarket.route('/supermarkets', methods=['GET', 'POST'])
+def get_supermarkets():
+    return render_template('all_supermarkets.html', supermarkets=get_data())
+
+
+@supermarket.route('/supermarkets', methods=['POST'])
+def search_supermarkets():
+    id_supermarket = request.form.get('id')
+    location = request.form.get('location')
+    data = []
+    for i in get_data():
+        if i['id'] == id_supermarket and i['location'] == location:
+            session[i['id']] = True
+            return render_template('all_supermarkets.html', supermarkets=i, link_flags=session)
+        elif i['location'] == location:
+            session[i['location']] = True
+            data.append(i)
+        elif i['id'] == id_supermarket:
+            session[i['id']] = True
+            data.append(i)
+    return render_template('all_supermarkets.html', supermarkets=data, link_flags=session)
+
+
+@supermarket.route('/<name_supermarket>', methods=['GET'])
+def get_supermarket(name_supermarket):
+    for i in supermarkets:
+        if i["name"] == name_supermarket:
+            return render_template('supermarket.html', picture=i['img_name'], title=i['name'], location=i['location'],
+                                   id_supermarket=i['id'])
+
+
+@supermarket.route('/add_supermarket', methods=['GET'])
+def add_supermarket():
+    return render_template('add_supermarket.html')
+
+
+@supermarket.route('/add_supermarket', methods=['POST'])
+def add_supermarket_post():
+    new_supermarket = {"id": str(uuid.uuid4()), "name": request.form.get('name'),
+                       "location": request.form.get('location')}
+    image = request.files['image']
+    new_supermarket["img_name"] = image.filename
+    path = os.path.join('blueprint/Supermarkets/static2', image.filename)
+    image.save(path)
+    supermarkets.append(new_supermarket)
+    add_data(supermarkets)
+    return "supermarket added"
+
+
