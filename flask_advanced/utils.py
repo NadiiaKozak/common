e15b755ddff9464fa0c79eb86830ee4b8eba8b06
Nadiia Kozak <lv191177anv@gmail.com>
2019-11-12 14:39:08 +0200

    add utils.py to advanced flask

--- flask_advanced/utils.py
+++ flask_advanced/utils.py
@@ -1,15 +1,17 @@
 import json
 
 
-
-
 def get_data(item_list):
-    with open(item_list) as file1:
-        return json.load(file1)
+    try:
+        with open(item_list) as files1:
+            return json.load(files1)
+    except(IOError, ValueError, FileNotFoundError, json.JSONDecodeError):
+        return []
 
 
 def add_data(data, item_list):
-    with open(item_list, 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
-
-
+    try:
+        with open(item_list, 'w', encoding='utf-8') as files1:
+            return json.dump(data, files1, ensure_ascii=False, indent=4)
+    except ValueError:
+        return item_list
