7ec7e5c4801f4172e1afc18ed506245c4ccfa0b4
Nadiia Kozak <42868703+NadiiaKozak@users.noreply.github.com>
2020-01-01 20:56:07 +0200

    Delete __init__.py
    delete file HW Flask4

--- flask_4/Rooms/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from flask_restful import Api
-
-from Rooms.rooms import Rooms
-
-api_rooms = Blueprint('rooms', __name__)
-api = Api(api_rooms)
-
-api.add_resource(Rooms, "/rooms")
